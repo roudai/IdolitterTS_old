0ac359d2e60633c72b6988331c26fa939c757ae5
roudai <me@roudai.net>
2022-09-28 00:13:59 +0900

    checkAccount実行メソッド実装

--- src/function-try.ts
+++ src/function-try.ts
@@ -1,3 +1,18 @@
+function checkAccount_try() {
+  var lastError = null;
+  for(var i = 0; i < 3; i++) {
+    try {
+      checkAccount();
+      return;
+    } catch(e) {
+      lastError = e;
+      Logger.log(e);
+    }
+    Utilities.sleep(10000);
+  }
+  throw lastError;
+}
+
 function dailyAnalysis_try(){
     var lastError = null;
     for(var i = 0; i < 10; i++) {
